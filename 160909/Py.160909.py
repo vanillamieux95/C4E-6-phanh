9f8d5ee59ef0f65060fe5a850ef5d32b47c4bdb6
PHANHPHANH\Phanh Phanh <ph.anh.nguyenpham.l1221@gmail.com>
2016-09-10 14:22:23 +0700

    hw

--- 160909/Py.160909.py
+++ 160909/Py.160909.py
@@ -56,18 +56,59 @@
 ##print (type(h))
 ##print (h)
 
-while True:
-    y = int(input("your year of birth"))
-    age = 2016 - y
-    print (age)
+##while True:
+##    y = int(input("your year of birth"))
+##    age = 2016 - y
+##    print (age)
+##
+##    if age < 6:
+##        print ("child")
+##    elif 6 <= age <= 18:
+##        print ("teenage")
+##    else:
+##        print ("not a child")
+##
+##    if  age == 18:
+##        print ("10 years old")
 
-    if age < 6:
-        print ("child")
-    elif 6 <= age <= 18:
-        print ("teenage")
+#Homework 1
+##w = int(input("Your Weight (kg): "))
+##h = float(input("Your Height (m): "))
+##bmi = w/h
+##print ("Your BMI: ", bmi)
+##
+##if bmi < 16:
+##    print ("You are Severely Under Weight")
+##elif 16 <= bmi <= 18.5:
+##    print ("You are Under Weight")
+##elif 18.5 < bmi <= 25:
+##    print ("You are Normal")
+##elif 25 < bmi <= 30:
+##    print ("You are Over-weight")
+##else:
+##    print ("You are Obesed")
+##
+
+#Homework 2
+from turtle import *
+#shape("turtle")
+speed(-1)
+
+for n in range (15, 1, -1):
+    if n % 2 == 0:
+        color("red","purple")
+        begin_fill()
+        for i in range (n):
+            forward(21)
+            left(180-180*(n-2)/n)
+        end_fill()
+
     else:
-        print ("not a child")
+        color("green", "yellow")
+##        begin_fill()
+##        for i in range (n):
+##            forward(21)
+##            left(180-180*(n-2)/n)
+##        end_fill()
 
-    if  age == 18:
-        print ("10 years old")
-    
+mainloop()
