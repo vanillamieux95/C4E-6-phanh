405bc4c9be8f4c89c2d76508a81781521e7c9e07
PHANHPHANH\Phanh Phanh <ph.anh.nguyenpham.l1221@gmail.com>
2016-09-18 15:18:55 +0700

    no message

--- 160916/Hw.160916.py
+++ 160916/Hw.160916.py
@@ -43,3 +43,24 @@ total = 0
 for k2 in prices:
     total = total + stock[k2] * prices[k2]
 print ("if we sold out all of them, we can earn in total: {0}".format(total))
+
+
+#### III ####
+groceries = ['banana', 'orange', 'apple']
+prices = {
+    'banana': 4,
+    'apple': 2,
+    'orange': 1.5,
+    'pear': 3
+    }
+stock = {
+    'banana': 6,
+    'apple': 0,
+    'orange': 32,
+    'pear': 15
+    }
+##total = 0
+##for x in groceries:
+##    def comput_bill(x):
+##        total = total + prices[x]
+##        return total
