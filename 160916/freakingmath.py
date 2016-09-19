63bc2fb622908854d432cc2a595df6bcb1af7561
Phanh Phanh <ph.anh.nguyenpham.l1221@gmail.com>
2016-09-19 11:05:25 +0700

    no message

---
+++ 160916/freakingmath.py
@@ -0,0 +1,42 @@
+#[self.num1, self.num2, self.sign, self.answer] = generate_quiz()
+#check_answer(self.num1, self.num2, self.sign, self.answer, True):
+
+from random import *
+def generate_quiz():
+    num1 = randint(0, 10)
+    num2 = randint(0, 20)
+    sign = choice(['+', '-', '*', '/'])
+    a = choice([num1+num2, num1-num2, num1*num2, num1/num2])
+    answer = a + choice([0,1,2])
+#    answer = choice([sign])
+    return num1, num2, sign, answer
+
+def check_answer(num1, num2, sign, answer, user_choice):
+    num1 = randint(0, 10)
+    num2 = randint(0, 20)
+    sign = choice(['+', '-', '*', '/'])
+    a = choice([num1+num2, num1-num2, num1*num2, num1/num2])
+    answer = a + choice([0,1,2])
+    user_choice == True or user_choice == False
+#    answer = choice([sign])
+    return num1, num2, sign, answer
+
+#user_choice = True
+    if answer == choice([num1+num2, num1-num2, num1*num2, num1/num2]):
+        if user_choice == True:
+            return True
+        elif user_choice != True:
+            return False
+        else:
+            print ("None")
+
+    elif answer != choice([num1+num2, num1-num2, num1*num2, num1/num2]):
+        if user_choice == True:
+            return False
+        elif user_choice != True:
+            return False
+        else:
+            print ("None")
+
+    else:
+        print ("None")
