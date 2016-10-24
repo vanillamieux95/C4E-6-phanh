2e4c54a611ff2717f4ee0aa099d4eb5e88e5d7ca
Phanh Phanh <ph.anh.nguyenpham.l1221@gmail.com>
2016-10-24 22:06:17 +0700

    changing website

--- retrieving-a-html/retrieving-dantri.py
+++ retrieving-a-html/retrieving-dantri.py
@@ -6,7 +6,7 @@ class HotNews:
         self.title = title
         self.link = link
 
-with urllib.request.urlopen('http://dantri.com.vn/') as response:
+with urllib.request.urlopen('https://www.fiverr.com/') as response:
    html = response.read()
    print(html)
 
@@ -17,27 +17,27 @@ with urllib.request.urlopen('http://dantri.com.vn/') as response:
    print(soup.title.string) #accessing value
    print(soup.title.name)
 
-   ul = soup.find("ul", "ul1 ulnew") #the closest tag that match
+   #ul = soup.find("ul", "ul1 ulnew") #the closest tag that match
 
    #print(ul[class]) #accessing attribute
-
-   li_list = ul.find_all("li")
-
-   hot_news_list = []
-
-   for li in li_list:
-       a = li.h4.a
-
-       hot_news = HotNews(a["title"],
-                          "http://dantri.com.vn" + a["href"]
-                          )
-       hot_news_list.append(hot_news)
-
-       print(a["title"])
-       print("http://dantri.com.vn" + a["href"])
-       print(".....................................................")
-   print(hot_news_list)
-
-   # f=open("dantri.html", "wb") # w = write, b= binary
-   # f.write(html)
-   # f.close()
+   #
+   # li_list = ul.find_all("li")
+   #
+   # hot_news_list = []
+   #
+   # for li in li_list:
+   #     a = li.h4.a
+   #
+   #     hot_news = HotNews(a["title"],
+   #                        "http://dantri.com.vn" + a["href"]
+   #                        )
+   #     hot_news_list.append(hot_news)
+   #
+   #     print(a["title"])
+   #     print("http://dantri.com.vn" + a["href"])
+   #     print(".....................................................")
+   # print(hot_news_list)
+   #
+   f=open("fiverr.html", "wb") # w = write, b= binary
+   f.write(html)
+   f.close()
